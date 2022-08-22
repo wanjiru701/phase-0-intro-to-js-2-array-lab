af7959bedcfecb100f88672a493fd1c595fb8389
whitney <whitney.njogu@student.moringaschool.com>
2022-08-22 17:32:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+function Arrayfunctions() {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  };
+  
+  function destructivelyAppendCat(name){
+    return cats.push(`Ralph`);
+  }
+  function destructivelyPrependCat(name){
+    return cats.unshift(`Bob`);
+  }
+  function destructivelyRemoveLastCat(){
+    return cats.pop(`Milo`);
+  }
+  function destructivelyRemoveFirstCat(){
+    return cats.shift(`Garfield`);
+  }
+  function appendCat(name){
+    const newpuss=[`Broom`];
+    return [`Milo`, `Otis`, `Garfield`, ...newpuss];
+  }
+  function prependCat(name){
+    return [`Arnold`,...cats];
+  }
+  function removeLastCat(){
+    return cats.slice(0,2);
+  }
+  function removeFirstCat(){
+    return cats.slice(1);
+  }
